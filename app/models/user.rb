7737d0611bdf460a46cf683d19742e1ef7cdb019
zoe472 <seiya1387@gmail.com>
2020-02-20 07:07:59 +0900

    aaa

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets
+  has_many :likes, dependent: :destroy
+  has_many :liked_tweets, through: :likes, source: :tweet
+  def already_liked?(tweet)
+    self.likes.exists?(tweet_id: tweet.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
+  namespace :tweets do
+    resources :searches, only: :index
+  end
   resources :searches, only: :index
-  resources :tweets
   resources :users
+  resources :tweets do
+    resources :likes, only: [:create, :destroy]
+  end
+  
 end
