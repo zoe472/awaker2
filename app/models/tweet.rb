b2ef5aa6ab4658a45531e1d3850efb237be289b1
zoe472 <seiya1387@gmail.com>
2020-02-21 06:32:23 +0900

    jj

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   validates :image, :category, :text, presence: true
 
